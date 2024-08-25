209aa68a924058a1dfedc93c5ab2a822071b91a3
Senghunkong <senghun672@gmail.com>
2024-08-25 15:14:19 +0700

    Create Person.php

---
+++ Person.php
@@ -0,0 +1,24 @@
+<?hph
+
+class Person{
+
+  public $Message;
+
+  public function showMessage($Msg)
+  {
+     $this->Message = $Msg
+    echo $this->$Message;
+    
+  }
+  
+  
+
+
+}
+
+
+
+
+
+
+  ?>
